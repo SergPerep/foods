289efcd18f93309d96c77f16789cd118692580f4
sergperep <perepelkin.serge@gmail.com>
2025-07-31 21:30:49 +0200

    Feat: Added validations for food DTO

--- DTOs/FoodDTO.cs
+++ DTOs/FoodDTO.cs
@@ -1,14 +1,26 @@
+using System.ComponentModel.DataAnnotations;
 using foods.Model;
 
 namespace foods.DTOs
 {
     public class FoodDTO
     {
+        [StringLength(100, MinimumLength = 3)]
         public required string Name { get; set; }
+
+        [Range(0, 100)]
         public int Protein { get; set; }
+
+        [Range(0, 100)]
         public int Carbs { get; set; }
+
+        [Range(0, 100)]
         public int Fat { get; set; }
+
+        [Range(0, 100)]
         public int Fiber { get; set; }
+
+        [Range(0, 100)]
         public int Alcohol { get; set; }
         public Food ToFood()
         {
