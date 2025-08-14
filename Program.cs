d13ffe90acbfeca1b30c5cea70bdc6beb9148062
sergperep <perepelkin.serge@gmail.com>
2025-08-14 08:13:18 +0200

    Fix: The app is not starting

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-using foods.Data;
+using foods.Database;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<FoodContext>(
+builder.Services.AddDbContext<DatabaseContext>(
     opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection"))
     );
 builder.Services.AddControllers();
