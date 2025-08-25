bea6e1b47545ba90d4d4f2017598d5f71254ebdb
sergperep <perepelkin.serge@gmail.com>
2025-08-25 08:58:57 +0200

    Feat: Arrange KeyVault for db connection string

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using foods.Database;
 using Microsoft.EntityFrameworkCore;
+
+var keyVaultUrl = "https://foods-key-vault.vault.azure.net/";
+var secretName = "postgres-connection-string";
+var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+KeyVaultSecret dbConnectionStringSecret = await client.GetSecretAsync(secretName);
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DatabaseContext>(
-    opt => opt.UseNpgsql(builder.Configuration["POSTGRESQLCONNSTR_PostgresConnection"])
+    opt => opt.UseNpgsql(dbConnectionStringSecret.Value)
     );
 builder.Services.AddControllers();
 
